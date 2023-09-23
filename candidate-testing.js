5ffb407646fd0a18a18a0f55d498ecf4a16c9bb2
stephonthomas <stephonthomas.edu@gmail.com>
2023-09-23 00:13:12 -0500

    candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,8 +11,8 @@ let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", 3];
 let candidateAnswers;
 
 
@@ -21,12 +21,16 @@ function askForName() {
 candidateName = input.question("Enter canidate name: ");
 }
 
-function askQuestion() {
+if (questions) = ["Who was the first American woman in space?", 
+"True or false: 5 kilometer == 5000 meters?", 
+"(5 + 3)/2 * 10 = ?", 
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 
+"What is the minimum crew size for the ISS?"]; 
+for (let i = 0; i < askQuestion, i++) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 candidateAnswer = input.question(question);
-
+console.log(questions[i]);
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
